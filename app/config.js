efc3fb671524962585f1e10a5a68b3162485dfd3
Steven Doan (BED) <109071123+MichaelTranBED@users.noreply.github.com>
2023-01-20 22:13:21 +0700

    fig base URL

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000/api"
-//export const BASE_URL = "https://page-bep-thi-thi-be.up.railway.app/api"
+//export const BASE_URL = "http://localhost:3000/api"
+export const BASE_URL = "https://page-bep-thi-thi.vercel.app/api"
 
 export const CLOUDINARY_CLOUD_NAME = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
 
